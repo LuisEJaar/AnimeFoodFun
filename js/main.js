41fddd8403462676d4469fe71fcf978af2a6426e
LuisEJaar <luisjaarcoding@gmail.ocm>
2022-02-23 11:18:38 -0600

    Initial creation

---
+++ js/main.js
@@ -0,0 +1,38 @@
+document.getElementById('meat').onclick = meat
+document.getElementById('pancake').onclick = pancake
+document.getElementById('ramen').onclick = ramen
+document.getElementById('ooze').onclick = ooze
+document.getElementById('omurice').onclick = omurice
+const content = document.querySelector(".content")
+
+const  tvOn = document.querySelector('.tvStatus')
+
+function common () {
+  tvOn.src = "./img/Old-Tube-Vector-Tv-EmptyScreen.png"
+  content.style.backgroundImage = "url(./img/static.gif)"
+}
+
+function meat() {
+  common()
+  setTimeout(() => {content.style.backgroundImage = "url(./img/Meat.gif)"}, 600);
+}
+
+function pancake () {
+  common()
+  setTimeout(() => {content.style.backgroundImage = "url(./img/Pancake.gif)"}, 600);
+}
+
+function ramen () {
+  common()
+  setTimeout(() => {content.style.backgroundImage = "url(./img/Ramen.gif)"}, 600);
+}
+
+function ooze () {
+  common()
+  setTimeout(() => {content.style.backgroundImage = "url(./img/Ooze.gif)"}, 600);
+}
+
+function omurice() {
+  common()
+  setTimeout(() => {content.style.backgroundImage = "url(./img/Omurice.gif)" }, 600);
+}
