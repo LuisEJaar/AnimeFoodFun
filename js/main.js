b43bac21fd8f2108b9ab8ce153c25aa2265f0e29
LuisEJaar <luisjaarcoding@gmail.ocm>
2022-02-23 18:30:02 -0600

    Updated with new name for common function to represent its purpose better. Added margin to omlette gif to center

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const content = document.querySelector(".content")
 const  tvOn = document.querySelector('.tvStatus')
-
-const knob = document.getElementById("knob")
+const tvBars = document.querySelector('.tvBars')
+const knob = document.getElementById('knob')
 knob.onclick = knobControl
 
 let rotation = 0;
@@ -30,39 +30,46 @@ function knobControl () {
   }
 }
 
-function common () {
+function static () {
   tvOn.src = "./img/Old-Tube-Vector-Tv-EmptyScreen.png"
   content.style.backgroundImage = "url(./img/static.gif)"
+  knob.style.marginTop = 0
+  tvBars.style.marginTop = 0
+  content.style.marginTop = 0
+  tvOn.style.marginTop = 0
 }
 
 function meat() {
-  common()
+  static()
   setTimeout(() => {content.style.backgroundImage = "url(./img/Meat.gif)"}, 600);
 }
 
 function pancake () {
-  common()
+  static()
   setTimeout(() => {content.style.backgroundImage = "url(./img/Pancake.gif)"}, 600);
 }
 
 function ramen () {
-  common()
+  static()
   setTimeout(() => {content.style.backgroundImage = "url(./img/Ramen.gif)"}, 600);
 }
 
 function ooze () {
-  common()
+  static()
   setTimeout(() => {content.style.backgroundImage = "url(./img/Ooze.gif)"}, 600);
 }
 
 function omurice() {
-  common()
+  static()
   setTimeout(() => {content.style.backgroundImage = "url(./img/Omurice.gif)" }, 600);
 }
 
 function omlette () {
-  common()
-  setTimeout(() => {content.style.backgroundImage = "url(./img/Omlette.gif)" }, 600);
+  static()
+  setTimeout(() => {content.style.backgroundImage = "url(./img/Omlette.gif)" 
+  knob.style.marginTop = '0'
+  tvBars.style.marginTop = '-4%'
+  content.style.marginTop = '4%'}, 600);
 }
 
 function off () {
